14f4ec29f6e0b374f04e357c4b6cd4d91c250675
cacicmia <cacic.miaa@gmail.com>
2019-06-03 22:29:42 +0200

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,26 @@ const gulp= require("gulp");
 const sass= require('gulp-sass');
 const babel = require('gulp-babel');
 sass.compiler = require('node-sass');
+const browserSync = require('browser-sync').create();
 //var babel = require("babel");
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server:{
+            baseDir: "./"
+        }
+    });
+});
 gulp.task('sass', function() {
     return gulp.src('./css/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
+/*
 gulp.task('babel', function(){
     gulp.src('./js/*.js')
     .pipe(babel({
         presets: ['@babel/preset-env']
     }))
     .pipe(gulp.dest('dist'))
-})
+})*/
+
